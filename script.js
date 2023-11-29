8322f23817f37b38b45a3f8c0924fe1d0f4a28b9
basswoman <54603761+basswoman@users.noreply.github.com>
2023-11-28 21:39:57 -0500

    Bass section

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* 
   When sections come into view we can read what amount are visible.
 */
-const intersectionHandler = (entries) => {
+const intersectionHandler = entries => {
   // could also use entries.forEach(... but for of loops are newer, fancier :)
   for (const entry of entries) {
     // The section has entered the screen
@@ -23,11 +23,11 @@ const intersectionHandler = (entries) => {
   Creates an I.O. and sets a threshold option with 1 -> 100.
   This allows us to detect what % of the section is in view 1% <-> 100%.
 */
-const observer = new IntersectionObserver(intersectionHandler, {
-  threshold: Array.from({ length: 100 }, (ignore, i) => (i + 1) / 100),
-});
+// const observer = new IntersectionObserver(intersectionHandler, {
+//   threshold: Array.from({ length: 100 }, (ignore, i) => (i + 1) / 100),
+// });
 
-// Observe all the section elements as the document scrolls
-document.querySelectorAll('section:not(#intro)').forEach((section) => {
-  observer.observe(section);
-});
+// // Observe all the section elements as the document scrolls
+// document.querySelectorAll('section:not(#intro)').forEach((section) => {
+//   observer.observe(section);
+// });
